99816d438b42eb73cb2026bf8e98833f0017f086
Lio10jr <kevinzambrano593@gmail.com>
2025-01-24 15:08:08 -0500

    Eliminación de guard para validar token jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { LoginAuthDto, RegisterAuthDto } from './dto';
-import { AuthGuard } from './guards/auth.guard';
 
 @Controller()
 export class AuthController {
