e483f1f23aec69a95f5e0a1768ac15654a0d124d
Lio10jr <kevinzambrano593@gmail.com>
2025-01-24 00:03:29 -0500

    Dto de actualización de información mejorado

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNumber()
+  @IsNotEmpty()
   id: number;
+
+  @IsOptional()
+  @IsString()
+  @IsEmail()
+  new_email: string;
+  
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  new_password: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(8)
+  repit_new_password: string;
 }
